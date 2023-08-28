a5f3c11628517f1773b24d26c849eff462694333
DanielHabenicht <daniel-habenicht@outlook.de>
2023-08-28 21:27:03 +0200

    fix server disconnected error handling

--- custom_components/livisi/aiolivisi.py
+++ custom_components/livisi/aiolivisi.py
@@ -114,15 +114,18 @@ class AioLivisi:
         self, method, url: str, payload=None, headers=None
     ) -> dict:
         """Send a request to the Livisi Smart Home controller and handle requesting new token."""
-        response = await self.__async_send_request(method, url, payload, headers)
-        if "errorcode" in response:
-            if response["errorcode"] == 2007:
-                await self.async_set_token()
-                response = await self.__async_send_request(
-                    method, url, payload, headers
-                )
-            else:
-                raise ErrorCodeException(response["errorcode"])
+        try:
+            response = await self.__async_send_request(method, url, payload, headers)
+        except Exception:
+            response = await self.__async_send_request(method, url, payload, headers)
+            if "errorcode" in response:
+                if response["errorcode"] == 2007:
+                    await self.async_set_token()
+                    response = await self.__async_send_request(
+                        method, url, payload, headers
+                    )
+                else:
+                    raise ErrorCodeException(response["errorcode"])
         return response
 
     async def __async_send_request(
