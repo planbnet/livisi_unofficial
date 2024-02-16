7f7057fdf2f41d139b03796effe67df71fbfad43
Felix Rotthowe <felix@planbnet.org>
2024-02-16 06:32:11 +0000

    Fix translation of sensor names

--- custom_components/livisi/sensor.py
+++ custom_components/livisi/sensor.py
@@ -198,7 +198,7 @@ class LivisiSensor(LivisiEntity, SensorEntity):
             use_room_as_device_name=(device.type in VRCC_DEVICE_TYPES),
         )
         self.entity_description = entity_desc
-        self._attr_translation_key = entity_desc.translation_key
+        self._attr_translation_key = entity_desc.key
 
     async def async_added_to_hass(self) -> None:
         """Register callbacks."""
@@ -247,6 +247,7 @@ class LivisiControllerSensor(LivisiEntity, SensorEntity):
             device,
         )
         self._attr_name = entity_desc.key
+        self._attr_translation_key = entity_desc.translation_key
         self._attr_unique_id = device.id + "_" + entity_desc.key
         self.entity_description = entity_desc
 
